86e46e8b3c421426fcb13da0a753bc2eedca484a
Jatin-Khanijoan <jatinkhanijoan@gmail.com>
2023-10-27 01:36:46 +0530

    tabs add

---
+++ lib/Add.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Add extends StatefulWidget {
+  const Add({Key? key}) : super(key: key);
+
+  @override
+  _AddState createState() => _AddState();
+}
+
+
+
+
+
+
+
+class _AddState extends State<Add> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+    );
+  }
+}
---
+++ lib/Analytics.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+import 'json/home_json.dart';
+import 'json/day_month.dart';
+import 'HomeTab.dart';
+
+
+class Analytics extends StatefulWidget {
+  const Analytics({Key? key}) : super(key: key);
+
+  @override
+  _AnalyticsState createState() => _AnalyticsState();
+}
+
+final data = <String, double>{
+  
+};
+
+class _AnalyticsState extends State<Analytics> {
+  List<MapEntry<String, double>> sortedData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Sort the data in descending order of amounts
+    sortedData = data.entries.toList()
+      ..sort((a, b) => b.value.compareTo(a.value));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: PieChart(
+              dataMap: data,
+              animationDuration: const Duration(milliseconds: 800),
+          chartLegendSpacing: 32,
+          chartRadius: MediaQuery.of(context).size.width / 2.2,
+          colorList: Colors.accents,
+          initialAngleInDegree: 0,
+          chartType: ChartType.ring,
+          ringStrokeWidth: 32,
+          centerText: "",
+          legendOptions: const LegendOptions(
+            showLegendsInRow: false,
+            legendPosition: LegendPosition.right,
+            showLegends: true,
+            legendShape: BoxShape.circle,
+            legendTextStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          chartValuesOptions: const ChartValuesOptions(
+            showChartValueBackground: true,
+            showChartValues: true,
+            showChartValuesInPercentage: false,
+            showChartValuesOutside: false,
+            decimalPlaces: 1,
+          ),
+
+              
+            ),
+          ),
+          
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: sortedData.length,
+            itemBuilder: (context, index) {
+              final entry = sortedData[index];
+              return ListTile(
+                title: Text(entry.key, style: TextStyle(fontSize: 20),),
+                subtitle: Text('\$${entry.value.toStringAsFixed(2)}',style: TextStyle(fontSize: 16)),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
